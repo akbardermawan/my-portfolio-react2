2930035987e5d1a2d9152384b583845dcbfe3b7d
galateaxixixiix <akbardermawan1998@gmail.com>
2025-05-14 08:13:53 +0700

    14/05/2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Resume from "./pages/Resume";
+import Blog from "./pages/Blog";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/blog" element={<Blog />} />
+    </Routes>
+  );
 }
 
 export default App;
